b5f59dadee7616b6fc41f8bd82980293a5ffd8b2
memorytao <memorytao@gmail.com>
2025-03-04 13:02:05 +0700

    remove timeout

--- src/api/getTrueLog.js
+++ src/api/getTrueLog.js
@@ -1,33 +1,30 @@
-const API_HOST = "http://localhost:8888/";
-const TIMEOUT = 10000;
+const API_HOST = "http://localhost:8888";
 
 export const getTrueResponseLogs = async (searchTerm) => {
-  await setTimeout(() => {
-    try {
-      const response = fetch(`${API_HOST}/apis/true/getResponseLog`, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          status: searchTerm.status,
-          msisdn: searchTerm.msisdn,
-          date: searchTerm.date,
-          packageCode: searchTerm.packageCode,
-        }),
-      });
+  try {
+    const response = await fetch(`${API_HOST}/apis/true/getResponseLog`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        status: searchTerm.status,
+        msisdn: searchTerm.msisdn,
+        date: searchTerm.date,
+        packageCode: searchTerm.packageCode,
+      }),
+    });
 
-      if (!response.ok) {
-        throw new Error(`HTTP error! Status: ${response.status}`);
-      }
-
-      const data = response.text(); // Assuming the API returns CSV data
-      return data;
-    } catch (err) {
-      console.error("Failed to fetch data:", err);
-      throw err; // Re-throw the error to handle it in the component
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
     }
-  }, TIMEOUT);
+
+    const data = await response.text(); // Assuming the API returns CSV data
+    return data;
+  } catch (err) {
+    console.error("Failed to fetch data:", err);
+    throw err; // Re-throw the error to handle it in the component
+  }
 };
 
 export const getTrueContactLogs = async (searchTerm) => {
